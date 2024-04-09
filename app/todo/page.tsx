f2d2a251a39e9d779cf2b9150da5c60ea622b072
kerubel98 <kirubeltadesse98@gmail.com>
2024-04-09 23:42:00 +0300

    therd day commit table added

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+const todos = [
+  {
+    description: "report",
+    dudet: "now",
+    status: "closed",
+    retated:"inbox",
+    action: "action",
+  },
+];
 
 const Todo = () => {
   return (
-    <div>todo</div>
-  )
-}
+    <Table>
+      <TableCaption>Taskes</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableCell>Task description</TableCell>
+          <TableCell>Due date</TableCell>
+          <TableCell>Sattues</TableCell>
+          <TableCell>Related To</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+        </TableHeader>
+        <TableBody>
+          {todos.map((to) => {
+            return (
+              <TableRow>
+                <TableCell>{to.description}</TableCell>
+                <TableCell>{to.dudet}</TableCell>
+                <TableCell>{to.status}</TableCell>
+                <TableCell>{to.retated}</TableCell>
+                <TableCell>{to.action}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+    </Table>
+  );
+};
 
-export default Todo
+export default Todo;
